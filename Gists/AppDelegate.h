22e9fdb1b43bebb4f8c1bcc02cba60028e7a51db
Simon Maddox <simon@simonmaddox.com>
2018-10-12 09:51:09 +0100

    Initial Commit

---
+++ Gists/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gists
+//
+//  Created by Simon Maddox on 12/10/2018.
+//  Copyright © 2018 Maddox Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Gists/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Gists
+//
+//  Created by Simon Maddox on 12/10/2018.
+//  Copyright © 2018 Maddox Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
